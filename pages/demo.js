0c69b5109c3c126f0c74727db68b943768808b92
eaTong <eatongchou@gmail.com>
2018-02-07 20:18:10 +0800

    add demo : m-preview-image

--- pages/demo.js
+++ pages/demo.js
@@ -21,6 +21,14 @@ class Demo extends Component {
               <li>
                 <a href="/static/demo/summary/index.html">个人工作总结</a>
                 <p className="content has-text-grey">2017年度工作总结及2018年度工作计划，采用 <code>impress.js</code>编制 </p>
+                <br/>
+              </li>
+              <li>
+                <a href="/static/demo/preview-image/index.html">m-preview-image</a>
+                <p className="content has-text-grey"><code>m-preview-image</code>使用样例。<code>m-preview-image</code>是一个
+                  纯js，不依赖任何库的小插件，用以在移动端进行图片预览。加入了平滑模式来保证体验性，并且为了保证不浪费流量，每次只加载相关的三个
+                  （当前图片，上一张，下一张），可以利用smoothly参数进行关闭。
+                </p>
               </li>
             </ul>
           </div>
