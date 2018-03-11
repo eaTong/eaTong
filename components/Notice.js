ea7bb4346c699fb228b0c7af5de06652467538f6
eaTong <eatongchou@gmail.com>
2018-03-11 09:33:08 +0800

    auto count days for my daughter

--- components/Notice.js
+++ components/Notice.js
@@ -4,9 +4,12 @@
 import React from 'react';
 
 const Notice = props => {
+  const birthday = new Date('2018-01-22 19:22:00');
+  const days = Math.round((new Date().getTime() - birthday.getTime()) / (1000 * 60 * 60 * 24));
   return (
     <div className="notification is-primary has-text-centered">
-      2018-01-22 喜得一千金!
+      <span>2018-01-22 喜得一千金! &nbsp;</span>
+      <span className="tag">{` ${days}天`}</span>
     </div>
   )
 };
